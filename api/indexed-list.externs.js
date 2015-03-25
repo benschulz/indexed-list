a1bf2c5d987fb63ba96edde5d59935b6c4b78146
benshu <benshu@benshu.de>
2015-03-25 15:27:38 +0100

    Added API.

---
+++ api/indexed-list.externs.js
@@ -0,0 +1,67 @@
+/**
+ * @constructor
+ * @template E
+ * @extends onefold.lists.List<E>
+ *
+ * @param {function(E):string} idSelector
+ */
+function IndexedList(idSelector) {}
+
+/**
+ * @returns undefined
+ */
+IndexedList.prototype.clear = function () {};
+
+/**
+ * @param {string} id
+ * @returns {E}
+ */
+IndexedList.prototype.getById = function (id) {};
+
+/**
+ * @param {string} id
+ * @returns {boolean}
+ */
+IndexedList.prototype.containsById = function (id) {};
+
+/**
+ * @param {Array<E>} elements
+ * @returns {undefined}
+ */
+IndexedList.prototype.removeAll = function (elements) {};
+
+/**
+ * @param {Array<string>} ids
+ * @returns {boolean}
+ */
+IndexedList.prototype.removeAllById = function (ids) {};
+
+/**
+ * @param {string} comparator
+ * @returns {E}
+ */
+IndexedList.prototype.sortBy = function (comparator) {};
+
+/**
+ * @param {Array<E>} updatedElements
+ * @returns {undefined}
+ */
+IndexedList.prototype.updateAll = function (updatedElements) {};
+
+/**
+ * @param {Array<E>} updatedElements
+ * @returns {Array<E>}
+ */
+IndexedList.prototype.tryUpdateAll = function (updatedElements) {};
+
+/**
+ * @param {Array<E>} newElements
+ * @returns {undefined}
+ */
+IndexedList.prototype.addAll = function (newElements) {};
+
+/**
+ * @param {Array<E>} newElements
+ * @returns {undefined}
+ */
+IndexedList.prototype.insertAll = function (newElements) {};
